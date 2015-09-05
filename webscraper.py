93a352e33ffd9cac6ba808620ee56b6a13955ca1
FazM <fm00021@gmail.com>
2015-09-05 16:56:38 +0100

    refractored code: modular

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,12 @@
 __author__ = 'Faz'
 from bs4 import BeautifulSoup
 import requests
-import pprint
 import pickle as cp
-
 
 # global variables
 game_apps = {}
 apps_page = 'https://steamdb.info/apps/'
+pickle_path = 'gameapps.pickle'
 
 # initialise first page
 html_Apps_page1 = requests.get(apps_page)
@@ -23,42 +22,48 @@ def find_no_of_pages(bs_Apps_page):
 #
 def web_page_generator(no_of_pages):
 
-    pages = [apps_page]
     for page_no in range (2, no_of_pages):
-        pages.append(apps_page + 'page{page_n}/'.format(page_n=str(page_no)))
+        yield (apps_page + 'page{page_n}/'.format(page_n=str(page_no)))
 
-    return pages
+#
+def generate_game_stats_from_url(url_list):
+    index = 0
+    for page_url in url_list:
+        html_Apps_page = requests.get(page_url)
+        bs_Apps_page = BeautifulSoup(html_Apps_page.text)
+        ts_Elements = bs_Apps_page.find_all("tr", class_="app")
 
+        for app in ts_Elements:
+
+            app_id, \
+            app_description, \
+            app_name, \
+            app_tstamp = [td_content.get_text() for td_content in app.find_all("td")]
+
+            if app_description == 'Game' and \
+                    app.find('a', attrs={'aria-label': 'This app is in store'}):
+
+                game_apps[app_id] = {}
+                game_apps[app_id]['app_name'] = app_name.replace('\n' , '')
+                game_apps[app_id]['app_tstamp'] = app_tstamp
+
+        index +=1
+        print('page {page_no} completed'.format(page_no=index))
+        print(len(game_apps))
+
+    return game_apps
+
+#
+def dump_to_pickle_file(object):
+    # lets dump our {} list in gameapps.pickle
+    with open(pickle_path, 'wb') as f:
+        cp.dump(object, f)
 
 no_of_pages = find_no_of_pages(bs_Apps_page1)
 page_urls = web_page_generator(no_of_pages)
+game_apps_dict = generate_game_stats_from_url(url_list = page_urls)
+dump_to_pickle_file(game_apps_dict)
 
-index = 0
 
-for page_url in page_urls:
-
-    html_Apps_page = requests.get(page_url)
-    bs_Apps_page = BeautifulSoup(html_Apps_page.text)
-    ts_Elements = bs_Apps_page.find_all("tr", class_="app")
-
-    for app in ts_Elements:
-
-        app_id, \
-        app_description, \
-        app_name, \
-        app_tstamp = [td_content.get_text() for td_content in app.find_all("td")]
-
-        if app_description == 'Game' and \
-                app.find('a', attrs={'aria-label': 'This app is in store'}):
-
-            game_apps[app_id] = {}
-            game_apps[app_id]['app_name'] = app_name.replace('\n' , '')
-            game_apps[app_id]['app_tstamp'] = app_tstamp
-
-    index +=1
-    print('page {page_no} completed'.format(page_no=index))
-    print(len(game_apps))
-
-# lets dump our {} list in gameapps.pickle
-with open('gameapps.pickle', 'wb') as f:
-    cp.dump(game_apps, f)
+# LOOK AT SUPER-PYTHON
+# LOOK AT GRABBING GAME TYPE FROM CHILD PAGES AND APP IDS I.E ACTION, PLATFORMER ECT
